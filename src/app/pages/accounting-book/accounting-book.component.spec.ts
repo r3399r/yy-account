20f88a6408c07df42f79a7bc81ea48b7bc3921c2
yue.chang <r3399r@gmail.com>
2020-11-02 12:27:41 +0800

    remove unused import

--- src/app/pages/accounting-book/accounting-book.component.spec.ts
+++ src/app/pages/accounting-book/accounting-book.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
 import { AccountingBookComponent } from 'src/app/pages/accounting-book/accounting-book.component';
